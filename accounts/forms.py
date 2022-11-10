2d18afd581345b3f2825f632cb9fa39a7e3d7f6a
Jaz <jkafsanchez@gmail.com>
2022-11-11 07:03:36 +0800

    test

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model, authenticate
 
 from django.forms import ModelForm
 from .models import RequestLeave
-import calculation
+
 
 User = get_user_model()
 
